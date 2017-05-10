3bb79304addea9fd655eb5099530fa3d8fc61525
Jose Zapata Zambrano <jose@6bitconsulting.com>
2017-05-10 11:35:15 -0400

    Create TodoApp component & update config files

--- app/app.jsx
+++ app/app.jsx
@@ -2,14 +2,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 // Load foundations-sites
-// require('style!css!foundation-sites/dist/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('!style-loader!css-loader!sass-loader!applicationStyles');
+require('!style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>Boilerplate 3 Project</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
